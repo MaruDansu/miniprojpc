e720ce9e846a595bd47281efafe3c3a718df5555
MaruDansu <mackung27@gmail.com>
2024-11-27 15:40:45 +0000

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import SneakerCard from './components/SneakerCard';
 
 export default function Home() {
+  
   const featuredSneakers = [
     { id: 1, name: "Air Jordan 1", price: 200, condition: "New", size: 10 },
     { id: 2, name: "Air Jordan 4", price: 250, condition: "Used", size: 9 },
@@ -10,6 +11,7 @@ export default function Home() {
 
   return (
     <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4 ">PALLADIUM</h1>
       <h1 className="text-2xl font-bold mb-4">Featured Sneakers</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {featuredSneakers.map((sneaker) => (
